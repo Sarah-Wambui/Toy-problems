0b5ddef31940e6bb3538427e42aca86fa6f6ec61
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-19 23:14:45 +0300

    another commit

--- script1.js
+++ script1.js
@@ -1,1 +1,25 @@
-let work = 50
+//Student Grade Generator
+//must be a function ,,,function key word
+//check  the input ,,,btwn 0 and 100
+//output to corresspond ,,,correct grades shown
+
+//pseudocode notes
+//write the function
+//declare variable
+// use if statements and console log the results
+
+function gradeGenerator(){
+    let mark = prompt("Enter Student Mark:");
+    if(mark > 79){
+        console.log ("Grade A")
+    }else if (mark >= 60 && mark <= 79){
+        console.log ("Grade B")
+    }else if (mark >= 50 && mark <= 59){
+        console.log ("Grade C") 
+    }else if (mark >= 40 && mark <= 49){
+        console.log ("Grade D")
+    }else {
+        console.log ("Grade E")
+    }
+}
+gradeGenerator() 
