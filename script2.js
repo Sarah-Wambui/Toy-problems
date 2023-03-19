d0ae15a4663377fede0fa1d2c2cc9c7213a2aa19
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-19 23:24:01 +0300

    add changes made

--- script2.js
+++ script2.js
@@ -1,1 +1,22 @@
-const test = 500;
+// Program that inputs speed of car
+//Speed less that 70 to print "Ok"
+//speed limit is 70
+//Every 5km/s above 70 get 1 demerit point
+// calculate demerit points
+// use a prompt
+
+function calculateSpeed(){
+    let speed= prompt("Enter Speed of Car:");
+    if ( speed < 70){
+        console.log("Ok")
+    }else {
+        //calculate demeritPoints for every speed above 70
+        demeritPoints = ((speed - 70) / 5)
+        if(demeritPoints > 12){
+            console.log ("Licence Suspended!")
+        }else {
+            console.log(`Your demeritPoints are ${demeritPoints}`)
+        }
+    }
+}
+calculateSpeed()
